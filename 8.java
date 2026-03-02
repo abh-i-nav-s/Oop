ffcc64d35aac5a2a5a1da425efb09a83712a9d95
abh-i-nav-s <chn25mca055@ceconline.edu>
2026-03-02 12:43:43 +0530

    Implement matrix addition in MatrixAdd class

---
+++ 8.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+class MatrixAdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int r, c;
+        System.out.print("Enter rows: ");
+        r = sc.nextInt();
+        System.out.print("Enter columns: ");
+        c = sc.nextInt();
+
+        int[][] A = new int[r][c];
+        int[][] B = new int[r][c];
+        int[][] C = new int[r][c];
+
+        System.out.println("Enter Matrix A:");
+        for (int i = 0; i < r; i++)
+            for (int j = 0; j < c; j++)
+                A[i][j] = sc.nextInt();
+
+        System.out.println("Enter Matrix B:");
+        for (int i = 0; i < r; i++)
+            for (int j = 0; j < c; j++)
+                B[i][j] = sc.nextInt();
+
+        for (int i = 0; i < r; i++)
+            for (int j = 0; j < c; j++)
+                C[i][j] = A[i][j] + B[i][j];
+
+        System.out.println("Result Matrix:");
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++)
+                System.out.print(C[i][j] + " ");
+            System.out.println();
+        }
+    }
+}
