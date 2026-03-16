7bfe511a5ac133405b34ad0828a6aa717c53e8f5
abh-i-nav-s <chn25mca055@ceconline.edu>
2026-03-16 13:27:01 +0530

    Update 14.java

--- 14.java
+++ 14.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Area {
 
     double area(double r) {
@@ -14,10 +16,43 @@ class Area {
 
     public static void main(String args[]) {
 
+        Scanner sc = new Scanner(System.in);
         Area obj = new Area();
 
-        System.out.println("Area of Circle = " + obj.area(5.0));
-        System.out.println("Area of Rectangle = " + obj.area(4.0,6.0));
-        System.out.println("Area of Square = " + obj.area(5));
+        System.out.println("Choose Shape");
+        System.out.println("1. Circle");
+        System.out.println("2. Rectangle");
+        System.out.println("3. Square");
+
+        System.out.print("Enter your choice: ");
+        int choice = sc.nextInt();
+
+        switch(choice) {
+
+            case 1:
+                System.out.print("Enter radius: ");
+                double r = sc.nextDouble();
+                System.out.println("Area of Circle = " + obj.area(r));
+                break;
+
+            case 2:
+                System.out.print("Enter length: ");
+                double l = sc.nextDouble();
+
+                System.out.print("Enter breadth: ");
+                double b = sc.nextDouble();
+
+                System.out.println("Area of Rectangle = " + obj.area(l,b));
+                break;
+
+            case 3:
+                System.out.print("Enter side: ");
+                int s = sc.nextInt();
+                System.out.println("Area of Square = " + obj.area(s));
+                break;
+
+            default:
+                System.out.println("Invalid choice");
+        }
     }
 }
